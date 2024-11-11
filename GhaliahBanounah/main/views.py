4f8fcaa43dff34f86262248a13abeb4e3b449cdf
Ghaliah <ghalia.banouna@gmail.com>
2024-11-11 15:27:46 +0300

    Base template styling

--- GhaliahBanounah/main/views.py
+++ GhaliahBanounah/main/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
 
+#Home view
 def homeView(request: HttpRequest):
     
     return render(request, 'main/home.html')
+
+#Mode change view
+def modeView(request: HttpRequest, mode):
+    response = redirect(request.GET.get("next", "/"))
+    
+    if mode == "light":
+        response.set_cookie("mode", "light")
+    elif mode == "dark":
+        response.set_cookie("mode", "dark")
+        
+    return response
