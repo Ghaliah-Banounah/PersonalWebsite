2b2984568596f5f52a67b7daeb195f5d885a6de6
Ghaliah <ghalia.banouna@gmail.com>
2024-11-12 13:45:47 +0300

    Add post Template and View

--- GhaliahBanounah/main/views.py
+++ GhaliahBanounah/main/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from interests.models import Interest
 from projects.models import Project
+from posts.models import Post
 
 #Home view
 def homeView(request: HttpRequest):
 
     projects = Project.objects.all().order_by('-createdAt')[0:2]
     interests = Interest.objects.all().order_by('-createdAt')[0:2]
+    posts = Post.objects.all().order_by('-publishedAt')[0:2]
 
-    return render(request, 'main/home.html', context={'interests': interests, 'projects': projects})
+    return render(request, 'main/home.html', context={'interests': interests, 'projects': projects, 'posts': posts})
 
 #Mode change view
 def modeView(request: HttpRequest, mode):
--- GhaliahBanounah/posts/views.py
+++ GhaliahBanounah/posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from .forms import PostForm
 
-# Create your views here.
+# Add post
+def addPostView(request: HttpRequest):
+
+    postData = PostForm()
+
+    response = render(request, 'posts/addPost.html')
+    
+    if request.method == "POST":
+        postData = PostForm(request.POST, request.FILES)
+        if postData.is_valid():
+            postData.save()
+            
+        response = redirect('main:homeView')
+        
+    return response
