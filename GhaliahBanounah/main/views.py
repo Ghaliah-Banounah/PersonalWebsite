36ed1c9efa23c6c39802523b188ce03879b6183e
Ghaliah <ghalia.banouna@gmail.com>
2024-11-12 22:07:33 +0300

    Fixed some issues with Contact Template and View

--- GhaliahBanounah/main/views.py
+++ GhaliahBanounah/main/views.py
@@ -23,20 +23,21 @@ def homeView(request: HttpRequest):
 def contactView(request: HttpRequest):
 
     contactData = ContactForm()
+
     response = render(request, 'main/contact.html')
-    
     if request.method == "POST":
 
         contactData = ContactForm(request.POST)
         if contactData.is_valid():
             contactData.save()
 
-            subject = "Planteer Support"
+            subject = "Regarding Your Message"
             fromEmail = settings.DEFAULT_FROM_EMAIL
             to = request.POST['email']
             htmlContent = render_to_string('main/mailTemplate.html', {'reciever': request.POST, 'sentAt': datetime.strftime( datetime.now() , "%d/%m/%Y, %H:%M:%S")})
             textContent = strip_tags(htmlContent)
             send_mail(subject, textContent, fromEmail, [to], html_message=htmlContent, fail_silently=False)
+        print(contactData.errors)
             
     return response
 
