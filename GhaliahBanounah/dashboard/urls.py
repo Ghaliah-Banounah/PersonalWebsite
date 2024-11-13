10f541f40ceede8d9254a53b4c827b1daea7aafe
Ghaliah <ghalia.banouna@gmail.com>
2024-11-13 13:21:28 +0300

    Started dashboard implementation

--- GhaliahBanounah/dashboard/urls.py
+++ GhaliahBanounah/dashboard/urls.py
@@ -5,4 +5,7 @@ app_name = "dashboard"
 
 urlpatterns = [
     path("", views.homeView, name="homeView"),
+    path("projects/", views.projectsDashView, name="projectsDashView"),
+    path("posts/", views.postsDashView, name="postsDashView"),
+    path("interests/", views.interestsDashView, name="interestsDashView"),
 ]
--- GhaliahBanounah/dashboard/views.py
+++ GhaliahBanounah/dashboard/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
-# Home page 
+# Dashboard home view 
 def homeView(request: HttpRequest):
 
-    return render(request, 'dashboard/home.html')
+    return render(request, 'dashboard/dashboard.html')
+
+# Porjects dashoard  
+def projectsDashView(request: HttpRequest):
+
+    return render(request, 'dashboard/projectsDash.html')
+
+# Posts dashboard 
+def postsDashView(request: HttpRequest):
+
+    return render(request, 'dashboard/postsDash.html')
+
+# Interests dahsboard 
+def interestsDashView(request: HttpRequest):
+
+    return render(request, 'dashboard/interestsDash.html')
