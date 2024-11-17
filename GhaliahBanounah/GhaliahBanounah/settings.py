38bd101a51002099aa40ea929fa9d315ba92374c
Ghaliah <ghalia.banouna@gmail.com>
2024-11-17 19:29:25 +0300

    Edits in settings.py

--- GhaliahBanounah/GhaliahBanounah/settings.py
+++ GhaliahBanounah/GhaliahBanounah/settings.py
@@ -146,8 +146,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = 'GhaliahBanounah/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'GhaliahBanounah/media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
